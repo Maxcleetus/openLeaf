74e8385667bf8f7d5ed20f634dc78cdbb775226c
max cleetus <maxcleetus@gmail.com>
2025-06-21 12:47:10 +0530

    project created

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,57 @@
+import Amclub5 from './pic/5amclub.jpg'
+import Harry1 from './pic/harry1.jpg'
+import richDad from './pic/richdad.jpg'
+import manju from './pic/manju.jpg'
+import deepWork from './pic/deepwork.jpg'
+
+
+//pdf
+
+import amclubpdf from './pdf/the-5-am-club.pdf'
+import harry1pdf from './pdf/1. Harry Potter and the Philosopher\'s Stone.pdf'
+import richdadpdf from './pdf/Rich Dad Poor Dad-compressed.pdf'
+import manjupdf from './pdf/4. Harry Potter and the Goblet of Fire.pdf'
+import deeppdf from './pdf/Deep Work Rules For Focused_ (Z-Library).pdf'
+
+
+export const details = [{
+    id:1,
+    name:"5 Am club",
+    image:Amclub5,
+    pdf:amclubpdf,
+    description:"very nice book",
+    auther:"max",
+    category:"novel"
+},{
+    id:2,
+    name:"Harry potter one",
+    image:Harry1,
+    pdf:harry1pdf,
+    description:"very nice book",
+    auther:"max",
+    category:"story"
+},{
+    id:3,
+    name:"Rich dad poor dad",
+    image:richDad,
+    pdf:richdadpdf,
+    description:"very nice book",
+    auther:"max",
+    category:"tech"
+},{
+    id:4,
+    name:"Manju",
+    image:manju,
+    pdf:manjupdf,
+    description:"very nice book",
+    auther:"max",
+    category:"notes"
+},{
+    id:5,
+    name:"deepwork",
+    image:deepWork,
+    pdf:deeppdf,
+    description:"very nice book",
+    auther:"max",
+    category:"tech"
+},]
