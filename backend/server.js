c0b8d93c14e0707f4dd4791229906b8b7fdc4159
max cleetus <maxcleetus@gmail.com>
2025-12-23 04:12:32 +0530

    backend deployment

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,7 @@ connectDB();
  connectCloudinary();
 
 
-app.listen(port, () => {
-  console.log("Server is running on port 3000");
-});
+// app.listen(port, () => {
+//   console.log("Server is running on port 3000");
+// });
+export default app;
